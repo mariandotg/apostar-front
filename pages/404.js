cdf75e22f6d12f76c605337ab9ecf69a5103f6f3
mariandotg <marianguillaume.m@gmail.com>
2022-09-16 16:22:38 -0300

    Añadidos los estilos

--- pages/404.js
+++ pages/404.js
@@ -2,8 +2,8 @@ import { Center, Text } from '@chakra-ui/react'
 
 export default function NotFound() {
   return (
-    <Center>
-      <Text>
+    <Center bg='apostar.red150' height="100vh">
+      <Text color="apostar.white" fontSize="50px">
         404 - Not Found
       </Text>
     </Center>
